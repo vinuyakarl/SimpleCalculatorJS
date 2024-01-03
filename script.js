22e38ad859be1a82f12af7b4703976a4272e1478
Karl Vinuya <karl_vinuya@yahoo.ca>
2024-01-03 15:18:40 -0700

    Finish calculator functionality

--- script.js
+++ script.js
@@ -1,161 +1,3 @@
-class Calculator {
-    constructor(input, output) {
-        this.inputDisplay = input;
-        this.outputDisplay = output;
-        this.inputHistory = [];
-    }
-
-    clearAllHistory() {
-        this.inputHistory = [];
-        this.updateInputDisplay();
-        this.updateOutputDisplay("0");
-    }
-
-    changePercentToDecimal() {
-        if (this.getLastInputType() === "number") {
-            this.editLastInput(this.getLastInputValue() / 100, "number");
-        }
-    }
-
-    insertNumber(value) {
-        if (this.getLastInputType() === "number") {
-            this.appendToLastInput(value);
-        }
-        else if (this.getLastInputType() === "operator" || this.getLastInputType() === null) {
-            this.addNewInput(value, "number");
-        }
-    }
-
-    insertOperation(value) {
-        switch (this.getLastInputType()) {
-            case "number":
-                this.addNewInput(value, "operator");
-                break;
-            
-            case "operator":
-                this.editLastInput(value, "operator");
-                break;
-
-            case "equals":
-                let output = this.getOutputValue();
-                this.clearAllHistory();
-                this.addNewInput(output, "number");
-                this.addNewInput(value, "operator");
-                break;
-            default:
-                return;
-        }
-    }
-
-    insertDecimalPoint() {
-        if (this.getLastInputType() === "number" && !this.getLastInputValue().includes(".")) {
-            this.appendToLastInput(".");
-        }
-        else if (this.getLastInputType() === "operator" || this.getLastInputType() === null) {
-            this.addNewInput("0.", "number");
-        }
-    }
-
-    negateNumber() {
-        if (this.getLastInputType() === "number") {
-            this.editLastInput(parseFloat(this.getLastInputValue()) * -1, "number");
-        }
-    }
-
-    generateResult() {
-        if (this.getLastInputType() === "number") {
-            const self = this;
-            const simplifyExpression = function(currentExpression, operator) {
-                if (currentExpression.indexOf(operator) === -1) {
-                    return currentExpression;
-                }
-                else {
-                    let operatorIndex = currentExpression.indexOf(operator);
-                    let leftOperandIndex = operatorIndex - 1;
-                    let rightOperandIndex = operatorIndex + 1;
-
-                    let partialSolution = self.performOperation(...currentExpression.slice(leftOperandIndex, rightOperandIndex + 1));
-                    currentExpression.slice(leftOperandIndex, 3, partialSolution.toString());
-
-                    return simplifyExpression(currentExpression, operator);
-                }
-            }
-
-            let result = ["x", "/", "+", "-"].reduce(simplifyExpression, this.getAllInputValues());
-
-            this.addNewInput("=", "equals");
-            this.updateOutputDisplay(result.toString());
-        }
-    }
-
-    // HELPER FUNCTIONS
-    getAllInputValues() {
-        return this.inputHistory.map(entry => entry.value);
-    }
-
-    updateInputDisplay() {
-        this.inputDisplay.value = this.getAllInputValues().join(" ");
-    }
-
-    updateOutputDisplay(value) {
-        this.outputDisplay.value = Number(value).toLocaleString();
-    }
-
-    getLastInputType() {
-        return this.inputHistory.length === 0 ? null : this.inputHistory[this.inputHistory.length - 1].type;
-    }
-
-    getLastInputValue() {
-        return this.inputHistory.length === 0 ? null : this.inputHistory[this.inputHistory.length - 1].value;
-    }
-
-    getOutputValue() {
-        return this.outputDisplay.value.replace(/,/g,'');
-    }
-    
-    addNewInput(value, type) {
-        this.inputHistory.push({"type": type, "value": value.toString()});
-        this.updateInputDisplay();
-    }
-
-    appendToLastInput(value) {
-        this.inputHistory[this.inputHistory.length - 1].value += value.toString();
-        this.updateInputDisplay();
-    }
-
-    editLastInput(value, type) {
-        this.inputHistory.pop();
-        this.addNewInput(value, type);
-    }
-
-    performOperation(leftOperand, operation, rightOperand) {
-        leftOperand = parseFloat(leftOperand);
-        rightOperand = parseFloat(rightOperand);
-
-        if (Number.isNaN(leftOperand) || Number.isNaN(rightOperand)) {
-            return;
-        } 
-
-        switch (operation) {
-            case "*":
-                return leftOperand * rightOperand;
-            
-            case "+":
-                return leftOperand + rightOperand;
-
-            case "-":
-                return leftOperand - rightOperand;
-            
-            case "/":
-                return leftOperand / rightOperand;
-
-            default:
-                return;
-        }
-    }
-
-}
-
 const inputDisplay = document.querySelector("#history");
 const outputDisplay = document.querySelector("#result");
 
@@ -167,38 +9,206 @@ const numberButtons = document.querySelectorAll("[data-number]");
 const decimalButton = document.querySelector("[data-decimal]");
 const equalsButton = document.querySelector("[data-equals]");
 
-const calculator = new Calculator(inputDisplay, outputDisplay);
+// Define variables to store the current and previous values and the current operation
+let currentInput = "";
+let wholeEquation = ""
+let previousInput = "";
+let result = "0"
+let currentOperation = null;
 
-clearButton.addEventListener("click", () => {
-    calculator.clearAllHistory();
-})
+// Function to update the display with the current input
+function updateDisplay() {
+  inputDisplay.value = wholeEquation;
+  outputDisplay.value = result;
+}
 
-percentButton.addEventListener("click", () => {
-    calculator.changePercentToDecimal();
-})
+// Function to handle number button clicks
+function handleNumberClick(number) {
+    if (wholeEquation.length <= 15) {
+        if (previousInput === "") {
+            previousInput = number.toString();
+            wholeEquation = number.toString();
+        } 
+        else if (previousInput !== "" && currentOperation == null) {
+            previousInput += number.toString();
+            wholeEquation += number.toString();
+        }
+        else {
+            currentInput += number.toString();
+            wholeEquation += number.toString();
+        }
+        updateDisplay();
+    }
+}
 
-operationButtons.forEach(button => {
-    button.addEventListener("click", (event) => {
-        let {target} = event;
-        calculator.insertOperation(target.dataset.operator);
-    })
-})
+// Function to handle operator button clicks
+function handleOperatorClick(operator) {
+    if (previousInput != "" && currentOperation == null && wholeEquation.length <= 15) {
+        currentOperation = operator;
+        wholeEquation += " " + operator + " ";
+        updateDisplay();
+    }
+    else if (previousInput != "" && currentOperation != null) {
+        currentOperation = operator;
+        wholeEquation = previousInput + " " + operator + " ";
+        updateDisplay();
+    }
+}
+
+// Calculates the numbers above
+function calculate() {
+    const num1 = parseFloat(previousInput);
+    const num2 = parseFloat(currentInput);
+    switch (currentOperation) {
+      case "+":
+        result = (num1 + num2);
+        break;
+      case "-":
+        result = (num1 - num2);
+        break;
+      case "*":
+        result = (num1 * num2);
+        break;
+      case "/":
+        if (num2 !== 0) {
+          result = (num1 / num2);
+        } else {
+          result = "Error";
+        }
+        break;
+      default:
+        break;
+    }
+    if (result != Math.floor(result)) {
+        result = parseFloat(result).toFixed(2);
+    }
+
+    if (result.length >= 8) {
+        result = parseFloat(result).toExponential(2);
+    }
+
+    currentOperation = null;
+    currentInput = "";
+    previousInput = result;
+    wholeEquation = result.toString();
+    if (result == "Error" || isNaN(result)) {
+        previousInput = "";
+        wholeEquation = "0";
+        result = "Error";
+    }
+    updateDisplay();
+  }
+
+  // Clears everything and resets calculator
+  function clearAll() {
+    currentInput = "";
+    wholeEquation = ""
+    previousInput = "";
+    result = "0"
+    currentOperation = null;
+    updateDisplay();
+  }
+
+  // Negates the number
+  function handleNegationClick() {
+    if (previousInput !== "" && currentOperation == null) {
+        previousInput = (parseFloat(previousInput) * -1).toString();
+        wholeEquation = previousInput;
+    }
+    else if (previousInput !== "" && currentOperation != null && currentInput !== "") {
+        currentInput = (parseFloat(currentInput) * -1).toString();
+        wholeEquation = previousInput + " " + currentOperation + " " + currentInput;
+    }
+
+    updateDisplay();
+  }
+
+  // Converts number to a percent
+  function handlePercentClick() {
+    if (previousInput !== "" && currentOperation == null) {
+        previousInput = (parseFloat(previousInput) / 100).toString();
+        wholeEquation = previousInput;
+    }
+    else if (previousInput !== "" && currentOperation != null && currentInput !== "") {
+        currentInput = (parseFloat(currentInput) / 100).toString();
+        wholeEquation = previousInput + " " + currentOperation + " " + currentInput;
+    }
+
+    updateDisplay();
+  }
+
+  // Adds a decimal point
+  function handleDecimalClick() {
+    if (previousInput !== "" && previousInput.indexOf(".") == -1 && currentOperation == null) {
+        previousInput += ".";
+        wholeEquation = previousInput;
+    }
+    else if (previousInput !== "" && currentOperation != null && currentInput !== "" && currentInput.indexOf(".") == -1) {
+        currentInput += ".";
+        wholeEquation = previousInput + " " + currentOperation + " " + currentInput;
+    }
+
+    updateDisplay();
+  }
+
 
 numberButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         let {target} = event;
-        calculator.insertNumber(target.dataset.number);
+        handleNumberClick(target.dataset.number);
     })
 })
 
-negateButton.addEventListener("click", () => {
-    calculator.negateNumber();
-})
+operationButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        let {target} = event;
+        handleOperatorClick(target.dataset.operator);
+    });
+  });
 
-decimalButton.addEventListener("click", () => {
-    calculator.insertDecimalPoint();
+clearButton.addEventListener("click", () => {
+    clearAll();
 })
 
 equalsButton.addEventListener("click", () => {
-    calculator.generateResult();
+    calculate();
+})
+
+negateButton.addEventListener('click', () => {
+    handleNegationClick();
+})
+
+percentButton.addEventListener('click', () => {
+    handlePercentClick();
+})
+
+decimalButton.addEventListener('click', () => {
+    handleDecimalClick();
+})
+
+
+// Keyboard listeners
+document.addEventListener('keydown', (event) => {
+    if (!isNaN(event.key)) {
+        handleNumberClick(event.key);
+    }
+    if (event.key == ".") {
+        handleDecimalClick();
+    }
+
+    if (["+", "-", "/", "*"].includes(event.key)) {
+        handleOperatorClick(event.key);
+    }
+
+    if (event.key == "%") {
+        handlePercentClick();
+    }
+
+    if (event.key == "Enter" || event.key == "=") {
+        calculate();
+    }
+
+    if (event.key == "Backspace") {
+        clearAll();
+    }
 })
